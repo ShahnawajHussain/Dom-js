94db13332a174c5b2bab630c7a1cdabd9e125c85
IamHussain0 <786shahnawajhussain07@gmail.com>
2024-12-16 11:10:24 +0530

    Update

---
+++ app.js
@@ -0,0 +1,15 @@
+//var titles = document.getElementsByClassName('title');
+//console.log(Array.isArray(titles));
+
+// //console.log(Array.isArray(Array.from(titles)));
+
+// Array.from(titles).forEach(function(item){
+//     console.log(item);
+// })
+
+// const wrap = document.querySelectorAll('#book-list li .name')
+// console.log(wrap)
+
+// let newParagraph = document.createElement('#page-banner .p');
+// newParagraph.textContent = "This is a new paragraph.";
+// document.body.appendChild(newParagraph);
